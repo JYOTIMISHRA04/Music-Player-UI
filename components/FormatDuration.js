5fae28b979afa94158ee4eaa216e009c909c22c1
Jyoti mishra <jyotimishra0896@gmail.com>
2024-09-03 11:23:29 +0530

    changes

--- components/FormatDuration.js
+++
@@ -1,9 +0,0 @@
-
-export const FormatDuration = (seconds) => {
-    if (isNaN(seconds) || seconds < 0) {
-        return '0:00';
-    }
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.floor(seconds % 60);
-    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-};
